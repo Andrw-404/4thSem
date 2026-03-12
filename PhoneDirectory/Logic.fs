6b47782e563c94dcfb146956d396d75f345ba559
Andrew Kalinin <Fipett2407@gmail.com>
2026-03-12 03:56:36 +0300

    add: processing an empty number

--- PhoneDirectory/Logic.fs
+++ PhoneDirectory/Logic.fs
@@ -5,7 +5,7 @@ let emptyData: (string * string) list = []
 let add (name: string) (phone: string) (db: (string * string) list) : Result<(string * string) list, string> =
     if name = "" then
         Error "Имя контакта пустое"
-    elif not (Seq.forall System.Char.IsDigit (phone.Replace("+", ""))) then
+    elif phone = "" || not (Seq.forall System.Char.IsDigit (phone.Replace("+", ""))) then
         Error "Телефон должен содержать только цифры и +"
     else
         Ok((name, phone) :: db)
