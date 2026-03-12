7dd119285345d75fd5c247af8b6fbc1d4d16b591
Andrew Kalinin <Fipett2407@gmail.com>
2026-03-12 15:51:42 +0300

    add: task with test

---
+++ PointFree/PointFree.Test/PointFreeFsCheck.fs
@@ -0,0 +1,11 @@
+﻿module PointFree.Test
+
+open NUnit.Framework
+open FsCheck
+
+[<Test>]
+let ``Point free версия должна вести себя как оригинал`` () =
+    let func0 x l = List.map (fun y -> y * x) l
+    let func6: int -> int list -> int list = List.map << (*)
+    let checker (x: int) (l: int list) = func0 x l = func6 x l
+    Check.QuickThrowOnFailure checker
---
+++ PointFree/Program.fs
@@ -0,0 +1,9 @@
+﻿module PointFree.task
+
+let func0 x l = List.map (fun y -> y * x) l
+let func1 (x: int) : (int list -> int list) = List.map (fun y -> y * x)
+let func2 (x: int) : (int list -> int list) = List.map (fun y -> x * y)
+let func3 (x: int) : (int list -> int list) = List.map (fun y -> (*) x y)
+let func4 (x: int) : (int list -> int list) = List.map ((*) x)
+let func5 (x: int) : (int list -> int list) = (List.map << (*)) x
+let func6: int -> int list -> int list = List.map << (*)
