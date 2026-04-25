137b2aa6a452636cf3363ebdf02cfd6254872586
Andrew Kalinin <Fipett2407@gmail.com>
2026-04-26 01:40:30 +0300

    test: upd

--- LocalWeb/LocalWeb.Tests/LocalWebTests.fs
+++ LocalWeb/LocalWeb.Tests/LocalWebTests.fs
@@ -5,60 +5,53 @@ open FsUnit
 open Network
 open Computer
 
-[<TestFixture>]
-type ``Tests``() =
+[<Test>]
+let ``Вирус не передается без соединений`` () =
+    let pc1 = Computer("PC1", Windows, true)
+    let pc2 = Computer("PC2", Windows, false)
 
-    [<Test>]
-    member this.``Вирус не передается без соединений``() =
-        let computers = [
-            { Id = "PC1"; OS = Windows; IsInfected = true }
-            { Id = "PC2"; OS = Windows; IsInfected = false }
-        ]
-        let connections = []
-        
-        let alwaysTrue = fun _ -> true
-        let network = Network(computers, connections, alwaysTrue)
-        
-        network.Step() |> should be False
-        let pc2 = network.Computers |> List.find (fun c -> c.Id = "PC2")
-        pc2.IsInfected |> should be False
+    let computers = [ pc1; pc2 ]
+    let connections = []
 
-    [<Test>]
-    member this.``При вероятности 1 обход в ширину``() =
-        let computers = [
-            { Id = "PC1"; OS = Windows; IsInfected = true }
-            { Id = "PC2"; OS = Windows; IsInfected = false }
-            { Id = "PC3"; OS = Windows; IsInfected = false }
-        ]
-        let connections = [ ("PC1", "PC2"); ("PC2", "PC3") ]
-        
-        let alwaysTrue = fun _ -> true
-        
-        let network = Network(computers, connections, alwaysTrue)
-        
-        network.Step() |> should be True
-        let state1 = network.Computers |> List.sortBy (fun c -> c.Id)
-        state1.[1].IsInfected |> should be True
-        state1.[2].IsInfected |> should be False
+    let alwaysTrue = fun _ -> true
+    let network = Network(computers, connections, alwaysTrue)
 
-        network.Step() |> should be True
-        let state2 = network.Computers |> List.sortBy (fun c -> c.Id)
-        state2.[2].IsInfected |> should be True
+    network.Step() |> should be False
+    pc2.IsInfected |> should be False
 
-        network.Step() |> should be False
+[<Test>]
+let ``При вероятности 1 обход в ширину`` () =
+    let pc1 = Computer("PC1", Windows, true)
+    let pc2 = Computer("PC2", Windows, false)
+    let pc3 = Computer("PC3", Windows, false)
 
-    [<Test>]
-    member this.``При вероятности 0 никто не заражается``() =
-        let computers = [
-            { Id = "PC1"; OS = Windows; IsInfected = true }
-            { Id = "PC2"; OS = Windows; IsInfected = false }
-        ]
-        let connections = [ ("PC1", "PC2") ]
-        
-        let alwaysFalse = fun _ -> false
-        
-        let network = Network(computers, connections, alwaysFalse)
-        
-        network.Step() |> should be False
-        let pc2 = network.Computers |> List.find (fun c -> c.Id = "PC2")
-        pc2.IsInfected |> should be False
+    let computers = [ pc1; pc2; pc3 ]
+    let connections = [ (pc1, pc2); (pc2, pc3) ]
+
+    let alwaysTrue = fun _ -> true
+
+    let network = Network(computers, connections, alwaysTrue)
+
+    network.Step() |> should be True
+    pc2.IsInfected |> should be True
+    pc3.IsInfected |> should be False
+
+    network.Step() |> should be True
+    pc3.IsInfected |> should be True
+
+    network.Step() |> should be False
+
+[<Test>]
+let ``При вероятности 0 никто не заражается`` () =
+    let pc1 = Computer("PC1", Windows, true)
+    let pc2 = Computer("PC2", Windows, false)
+
+    let computers = [ pc1; pc2 ]
+    let connections = [ (pc1, pc2) ]
+
+    let alwaysFalse = fun _ -> false
+
+    let network = Network(computers, connections, alwaysFalse)
+
+    network.Step() |> should be False
+    pc2.IsInfected |> should be False
